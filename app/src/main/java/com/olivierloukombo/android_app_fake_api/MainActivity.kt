523c159db534bf3eb6062bd8b20530e885593d63
Olivier Loukombo <olivierloukombo@gmail.com>
2022-12-16 13:30:25 +0100

    initial commit

---
+++ app/src/main/java/com/olivierloukombo/android_app_fake_api/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.olivierloukombo.android_app_fake_api
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.olivierloukombo.android_app_fake_api.ui.theme.AndroidappfakeapiTheme
+import com.olivierloukombo.android_app_fake_api.vm.MainViewModel
+
+class MainActivity : ComponentActivity() {
+    val viewModel  by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidappfakeapiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CommentList(viewModel.commentListResponse)
+                    viewModel.getComments()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CommentList(comments: List<Comment>) {
+    LazyColumn {
+        itemsIndexed(items = comments) { _, item ->
+            CommentItem(commentMe = item)
+        }
+    }
+}
+
+@Composable
+fun CommentItem(commentMe: Comment) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp, 4.dp)
+            .fillMaxWidth()
+            .height(110.dp), shape = RoundedCornerShape(8.dp), elevation = 4.dp
+    ) {
+        Surface() {
+
+            Row(
+                Modifier
+                    .padding(4.dp)
+                    .fillMaxSize()
+            ) {
+                
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxHeight()
+                        .weight(0.8f)
+                ) {
+                    Text(
+                        text = commentMe.name,
+                        style = MaterialTheme.typography.subtitle1,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = commentMe.email,
+                        style = MaterialTheme.typography.caption,
+                        modifier = Modifier
+                            .padding(4.dp)
+
+                    )
+
+                    Text(
+                        text = commentMe.body,
+                        style = MaterialTheme.typography.body1,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
+                    )
+
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidappfakeapiTheme {
+        val dummy = Comment(
+            "id labore ex et quam laborum",
+            "example@example.com",
+            "laudantium enim quasi est quidem magnam voluptate ipsam eos\\ntempora quo necessitatibus\\ndolor quam autem quasi\\nreiciendis et nam sapiente accusantium"
+        )
+       CommentItem(dummy)
+    }
+}
