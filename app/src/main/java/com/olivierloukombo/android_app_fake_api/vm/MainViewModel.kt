0526f9598cacc6e1faa2a126c72d066be46a4a70
Olivier Loukombo <olivierloukombo@gmail.com>
2022-12-17 17:40:16 +0100

    apply di principe with dagger hilt

--- app/src/main/java/com/olivierloukombo/android_app_fake_api/vm/MainViewModel.kt
+++ app/src/main/java/com/olivierloukombo/android_app_fake_api/vm/MainViewModel.kt
@@ -5,26 +5,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.olivierloukombo.android_app_fake_api.network.ApiService
 import com.olivierloukombo.android_app_fake_api.model.Comment
+import com.olivierloukombo.android_app_fake_api.repository.CommentRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(private val repository: CommentRepository) : ViewModel() {
 
-    var commentListResponse: List<Comment> by mutableStateOf(listOf())
-    var error: String by mutableStateOf("")
-    fun getComments(): List<Comment>{
+    private var commentListResponse: List<Comment> by mutableStateOf(listOf())
+
+    fun getComments(): List<Comment> {
         viewModelScope.launch {
-            val api = ApiService.getInstance()
-            try {
-                val comments = api.getComments()
-                commentListResponse = comments
-            }catch (e: Exception){
-                error = e.message.toString()
-            }
+            val comments = repository.getComments()
+            commentListResponse = comments
         }
         return commentListResponse;
     }
